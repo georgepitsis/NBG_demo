5d82e4e51118aab39a0d6d2be9fb2691c848c4f3
George Pitsis <george.pitsis@gr.ey.com>
2024-07-27 15:15:38 +0300

    Add greeting module with time-based greetings

---
+++ greeting.py
@@ -0,0 +1,11 @@
+# greeting.py
+import datetime
+
+def get_greeting():
+    hour = datetime.datetime.now().hour
+    if hour < 12:
+        return "Good Morning!"
+    elif 12 <= hour < 18:
+        return "Good Afternoon!"
+    else:
+        return "Good Evening!"
